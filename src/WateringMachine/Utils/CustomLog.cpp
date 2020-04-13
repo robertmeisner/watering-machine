de8bf8b7c0a449d80db2dc0caa183eb22c20a919
Robert Meisner <robert@catchit.pl>
2020-04-13 17:07:45 +0200

    adds debug messaging filtering related to #1

--- src/WateringMachine/Utils/CustomLog.cpp
+++ src/WateringMachine/Utils/CustomLog.cpp
@@ -19,30 +19,45 @@ inline const char *DebugLevelToString(DebugLevel v)
         return "[Unknown DebugLevel]";
     }
 }
+bool cLogShowDebugLevel(DebugLevel v)
+{
+    if (v <= currentDebugLevel)
+        return true;
+    else
+        return false;
+}
 void cLog(const char *message, DebugLevel v /*=DebugLevel::INFO*/)
 {
+    if (!cLogShowDebugLevel)
+        return;
     String str = "";
     str += DebugLevelToString(v);
     str += " - ";
     str += message;
     Serial.println(str);
-    
 }
-void cLog(const String message,  DebugLevel v /*=DebugLevel::INFO*/)
+void cLog(const String message, DebugLevel v /*=DebugLevel::INFO*/)
 {
+    if (!cLogShowDebugLevel)
+        return;
     String str = "";
     str += DebugLevelToString(v);
     str += " - ";
     str += message;
     Serial.println(str);
-    
 }
-void cLog(const std::string& message, DebugLevel v /*=DebugLevel::INFO*/)
+void cLog(const std::string &message, DebugLevel v /*=DebugLevel::INFO*/)
 {
+    if (!cLogShowDebugLevel)
+        return;
     String str = "";
     str += DebugLevelToString(v);
     str += " - ";
     str = message.c_str();
     Serial.println(str);
-    
+}
+
+void cLogsetDebugLevel(DebugLevel v)
+{
+    currentDebugLevel = v;
 }
--- src/WateringMachine/Utils/CustomLog.h
+++ src/WateringMachine/Utils/CustomLog.h
@@ -1,19 +1,40 @@
 #ifndef CustomDebug_h
 #include <Arduino.h>
 #define CustomDebug_h
-enum DebugLevel
+enum class DebugLevel : unsigned char
 {
-    INFO,
-    VERBOSE,
-    WARNING,
-    ERROR,
-    DEBUG,
-    FATAL,
-    ALL,
-    OFF
+    OFF = 0x01,
+    FATAL = 0x03,
+    ERROR = 0x05,
+    WARNING = 0x10,
+    INFO = 0x20,
+    DEBUG = 0x40,
+    VERBOSE = 0x80,
+    ALL = 0x99
 };
+static DebugLevel currentDebugLevel = DebugLevel::INFO;
 inline const char *DebugLevelToString(DebugLevel v);
-void cLog(const char *message, DebugLevel v=DebugLevel::INFO);
-void cLog(const String message, DebugLevel v=DebugLevel::INFO);
-void cLog(const std::string& message, DebugLevel v=DebugLevel::INFO);
+/**
+ * Sets a Debug Level. Used to diable  logging of low level messages.
+ * @param  {DebugLevel} v : 
+ */
+void cLogSetDebugLevel(DebugLevel v);
+/**
+ * 
+ * @param  {char*} message : 
+ * @param  {DebugLevel} v  : 
+ */
+void cLog(const char *message, DebugLevel v = DebugLevel::INFO);
+/**
+ * 
+ * @param  {String} message : 
+ * @param  {DebugLevel} v   : 
+ */
+void cLog(const String message, DebugLevel v = DebugLevel::INFO);
+/**
+ * 
+ * @param  {std::string} message : 
+ * @param  {DebugLevel} v        : 
+ */
+void cLog(const std::string &message, DebugLevel v = DebugLevel::INFO);
 #endif
